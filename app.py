1c13745b26da61ec9ab9bf2ff63a10f1e9a82527
Anvar Azimboev <155817633+AbuPythonIbnPascal@users.noreply.github.com>
2024-08-03 16:02:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,19 @@ def add_task():
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.route('/complete/<int:task_id>')
+def complete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    task.completed = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 if __name__=='__main__':
     app.run(debug=True)
